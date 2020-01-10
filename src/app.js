52048f8959558b89d58174b395714161fbc03708
ThaiNgan <truongthaingan19@gmail,com>
2020-01-11 00:16:13 +0700

    Version0.2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import "./sass/main.scss";
 // import 'slick-carousel';
 import 'slick-carousel/slick/slick';
+import $ from 'jquery';
 import 'jquery';
 // import { addJs } from "./js/index.js";
-const carousel = document.querySelector(".owl-carousel");
-carousel.slick({
-    infinite: true
+// const carousel = document.querySelector(".owl-carousel");
+$(".owl-carousel").slick({
+    infinite: true,
+    autoplaySpeed: 3000,
+    // autoplay: true,
+    arrows:true,
+    prevArrow: '<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fas fa-chevron-right"></i></div>',
 })
 // addJs();
+$("document").ready()
 
 const icons_menu = document.querySelector(".icons-menu");
 const mini_menu = document.querySelector(".menu-mini");
@@ -20,32 +27,40 @@ icons_exit.addEventListener('click', function(){
     mini_menu.classList.remove('active');
 });
 
-const carousel_next = document.querySelector('.carousel .fa-chevron-right');
-const carousel_pre = document.querySelector('.carousel .fa-chevron-left');
 
-var carousel_index = 0;
+const list_zoomIn = document.querySelectorAll(".ZoomOut-editor .prod-ZOE");
+const list_zoomOut = document.querySelectorAll(".zoomIn-editor .prod-ZIE");
+var index_news = 0;
+for (let index = 0; index < list_zoomIn.length; index++) {
+    const element = list_zoomIn[index];
+    element.addEventListener('mouseover', function(){ 
+        addActiveClass(index);
+        changeZoomIn();
+    })
+}
 
-// carousel_next.addEventListener('click',function(){
-//     carousel_index ++;
-//     carousel_func() ;
-// })
+function addActiveClass(index){
+    list_zoomIn.forEach(element => {
+        element.classList.remove('active');
+    });
+    index_news = index;
+    list_zoomIn[index_news].classList.add("active");
+}
 
+function changeZoomIn() {
+    list_zoomOut.forEach(element =>{
+        element.style.display = "none";
+    })
+    list_zoomOut[index_news].style.display = "block";
+}
 
-// carousel_pre.addEventListener('click',function(){
-//     carousel_index --;
-//     carousel_func() ;
-// })
-
-// function carousel_func(){
-//     if(carousel_index >=2){
-//         carousel_index = 0;
-//     }
-//     if(carousel_index <=-1){
-//         carousel_index = 1;
-//     }
-//     if(carousel_index == 0){
-//         carousel.classList.remove("active_2");
-//     }
-//     else
-//         carousel.classList.add("active_2");
-// }
+setInterval(function(){
+    if(index_news+1 <3){
+        addActiveClass(index_news+1);
+        changeZoomIn();
+    }
+    else{
+        addActiveClass(0);
+        changeZoomIn();
+    }
+}, 3000);
